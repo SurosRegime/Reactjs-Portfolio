4c00ca7f02dfb269154d7011d9d310d2b93839ad
SurosRegime <ephrain926@hotmail.com>
2023-05-28 23:55:16 -0400

    create about.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <img src="avatar.png" alt="Developer" />
+      <p>Short bio about the developer...</p>
+    </section>
+  );
+}
+
+export default About;
